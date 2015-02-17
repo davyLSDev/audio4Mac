ddf7cacb0996364db03df290a7b8bd1374ffba9c
Davey Tenn <davy.lsdev@gmail.com>
2015-02-16 18:53:43 -0700

    Setup a basic play, pause, resume audio player

--- audio4Mac/AudioInterface.cs
+++ audio4Mac/AudioInterface.cs
@@ -5,19 +5,28 @@
  * Miquel and Mike Hutchitson give info here:
  * http://stackoverflow.com/questions/3880148/can-i-use-osx-augraph-from-monomac
  */
-
-using System;
+// CHECK some of these may not be needed!
+using MonoMac;
+using MonoMac.Foundation;
+using MonoMac.AVFoundation;
+using MonoMac.AppKit;
+// using MonoMac.AudioUnitWrapper;
+using MonoMac.AudioToolbox;
+using MonoMac.AudioUnit;
 
 namespace SimpleAudio
 {
 	public class AudioInterface
 	{
 		public int ERR = 0;
+		public NSSound playback = new NSSound ("../../media/applause_y.wav", byRef: false);
 		public AudioInterface ()
 		{
+			NSApplication.Init ();
 		}
 		public int Play()
 		{
+			playback.Play ();
 			return ERR;
 		}
 		public int Record()
@@ -26,10 +35,12 @@ namespace SimpleAudio
 		}
 		public int Pause()
 		{
+			playback.Pause ();
 			return ERR;
 		}
 		public int  Resume()
 		{
+			playback.Resume ();
 			return ERR;
 		}
 	}
--- audio4Mac/AudioUI.cs
+++ audio4Mac/AudioUI.cs
@@ -4,15 +4,6 @@ using System.Windows.Forms;
 using System.Drawing;
 using System.Media;
 
-// using IrrKlang;
-using MonoMac;
-using MonoMac.Foundation;
-using MonoMac.AVFoundation;
-using MonoMac.AppKit;
-// using MonoMac.AudioUnitWrapper;
-using MonoMac.AudioToolbox;
-using MonoMac.AudioUnit;
-
 namespace SimpleAudio {
 	class MForm : Form {
 		AudioInterface Testing = new AudioInterface();
@@ -124,41 +115,3 @@ namespace SimpleAudio {
 	}
 
 }
-
-/* taken from irrKlang .net examples example 1:
- * using System;
-using IrrKlang;
-
-namespace CSharp._01.HelloWorld
-{
-	class Class1
-	{
-		[STAThread]
-		static void Main(string[] args)
-		{
-			// start the sound engine with default parameters
-			ISoundEngine engine = new ISoundEngine();
-
-			// To play a sound, we only to call play2D(). The second parameter
-			// tells the engine to play it looped.
-
-			engine.Play2D("../../media/getout.ogg", true);
-
-			Console.Out.WriteLine("\nHello World");
-
-			do
-			{
-				Console.Out.WriteLine("Press any key to play some sound, press 'q' to quit.");
-
-				// play a single sound
-				engine.Play2D("../../media/bell.wav");
-			}
-			while(_getch() != 'q');
-		}
-		
-		// some simple function for reading keys from the console
-		[System.Runtime.InteropServices.DllImport("msvcrt")]
-		static extern int _getch();
-	}
-}
-*/
